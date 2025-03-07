3fd1a755a90d847e66ff4ba5b9641eb0fd1711e2
Maximiliano Munoz <maximiliano_munoz@epam.com>
2025-03-07 17:14:09 -0300

    init commit

--- oci_bucket/asserts.tf
+++ oci_bucket/asserts.tf
@@ -3,4 +3,8 @@ check "bucket" {
         condition = length(oci_objectstorage_bucket.bucket.name) >= 3 && length(oci_objectstorage_bucket.bucket.name) <= 63
         error_message = "Bucket name must be between 3 and 63 characters"
     }
+    assert {
+        condition = can(regex("^([a-z0-9][-a-z0-9.]{1,61}[a-z0-9])$", oci_objectstorage_bucket.bucket.name))
+        error_message = "Bucket name must start with a lowercase letter or number and can only contain lowercase letters, numbers, hyphens, and periods."
+    }
 }
